1dfb07a6f18382805f1d4092977a590d5b625e61
seon <tjswlghks0722@naver.com>
2025-04-30 21:26:26 +0900

    Initial commit

---
+++ src/main/java/com/seon/fairin/FairInApplication.java
@@ -0,0 +1,13 @@
+package com.seon.fairin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FairInApplication.class, args);
+    }
+
+}
